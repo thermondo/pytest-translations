2be74ea7c1b02c99c86ba3a4c089ba5f1021eef8
Denis Cornehl <syphar@fastmail.fm>
2016-04-25 10:51:27 +0200

    bump version and release

--- pytest_translations/__init__.py
+++ pytest_translations/__init__.py
@@ -5,7 +5,7 @@ from __future__ import (absolute_import, division, print_function,
 from .mo_files import MoFileItem
 from .po_files import PoFile
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 
 def pytest_addoption(parser):
